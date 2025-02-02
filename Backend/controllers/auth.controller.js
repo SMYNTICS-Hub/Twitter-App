3ad27130b585847663e287e0355e19cb2b47d60d
SMYNTICS-hub <smynticscorp@gmail.com>
2025-02-02 21:09:02 +0530

    3. Basic signup function implemented

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,7 +1,44 @@
+import mongoose from "mongoose";
+import User from "../models/userModel";
+import bcrypt from "bcryptjs";
+
 export const signup = async (req, res) => {
-  res.json({
-    message: "Signup successful!",
-  });
+  try {
+    const [username, email, fullname, password] = req.body;
+
+    //email validation
+    let regx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (email != regx.test) {
+      return res.status(400).json({ error: "The email is not valid!" });
+    }
+
+    //userProfile validation
+    const existingUser = await mongoose.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ error: "The username is already taken!" });
+    }
+
+    //emailProfile validation
+    const existingEmail = await mongoose.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ error: "The email is already taken!" });
+    }
+
+    //Hashing password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = bcrypt.hash(password, salt);
+
+    //creating new user
+    newUser = new User({
+      fullname,
+      username,
+      email,
+      hashedPassword,
+    });
+  } catch (error) {
+    console.log("Error in signup controller");
+    return res.status(500).json({ error: "Internal server error!" });
+  }
 };
 
 export const login = async (req, res) => {
