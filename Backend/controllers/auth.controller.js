0f4e865660e5a0f6bb06d54feed8003151669222
SMYNTICS-hub <smynticscorp@gmail.com>
2025-03-08 19:23:47 +0530

    3.3 Implement basic logout function

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -90,4 +90,13 @@ export const login = async (req, res) => {
   }
 };
 
-export const logout = async (req, res) => {};
+export const logout = async (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    console.log("Logged out successfully!");
+    res.status(200).json({ message: "Logged out successfully!" });
+  } catch (error) {
+    console.log("Error in logout. \nError: " + error);
+    return res.status(500).json({ error: "Internal server error! " + error });
+  }
+};
